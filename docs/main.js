03cb8a71943d1a184d084a3d3431ac8ca25429af
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-04-27 15:13:30 +0900

    commit

--- docs/main.js
+++ docs/main.js
@@ -60,12 +60,14 @@ btnRecord.onclick = evt => {
     if (btnRecord.textContent === 'record') {
         btnRecord.textContent = 'stop';
         recordChunks = [];
-        mediaRecorder = new MediaRecorder(remoteStream, {mimeType: 'video/webm; codecs=vp8'});
+        mediaRecorder = new MediaRecorder(remoteStream, { mimeType: 'video/webm; codecs=vp8' });
         mediaRecorder.ondataavailable = evt => {
-            recordChunks.push(evt.data);
+            if (evt.data && evt.data.size > 0) {
+                recordChunks.push(evt.data);
+            }
         };
         mediaRecorder.onstop = evt => {
-            const blob = new Blob(recordChunks);
+            const blob = new Blob(recordChunks, {type: 'video/webm'});
             recordView.src = URL.createObjectURL(blob);
         };
         mediaRecorder.start(10);
