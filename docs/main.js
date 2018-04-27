eac2813ea9a1d4f91cd15969c60526afed813d4d
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-04-27 14:14:51 +0900

    commit

---
+++ docs/main.js
@@ -0,0 +1,72 @@
+const peer = new Peer({ key: 'cc1edbd6-1f11-48ab-9680-f2a5f74633b4' });
+let myId = null;
+let remoteId = null;
+let localStream = null;
+let remoteStream = null;
+let mediaConnection = null;
+let mediaRecorder = null;
+let recordChunks = [];
+const snapshotCnv = document.createElement('canvas');
+const snapshotCtx = shapshotCnv.getContext('2d');
+btnConnect.disabled = btnRecord = true;
+
+peer.on('open', id => {
+    myId = id;
+    peer.listAllPeers(peers => {
+        remoteId = peers.filter(peerId => myId !== myId)[0];
+        if (remoteId) {
+            btnConnect.disabled = false;
+        }
+    })
+});
+
+peer.on('call', async call => {
+    callEventHandler(call);
+    await getLocalStream();
+
+});
+
+btnConnect.onclick = async evt => {
+    await getLocalStream();
+    peer.call(remoteId, localStream);
+};
+
+async function getLocalStream() {
+    try {
+        localStream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: false
+        });
+        localView.srcObject = localStream;
+        localView.play();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function callEventHandler(call) {
+    call.on('stream', stream => {
+        remoteStream = stream;
+        remoteView.srcObject = stream;
+        remoteView.play();
+        btnRecord.disabled = false;
+    });
+}
+
+btnRecord.onclick = evt => {
+    if (btnRecord.textContent === 'record') {
+        btnRecord.textContent = 'stop';
+        recordChunks = [];
+        mediaRecorder = new MediaRecorder(remoteStream);
+        mediaRecorder.ondataavailable = evt => {
+            chunks.push(evt.data);
+        };
+        mediaRecorder.onstop = evt => {
+            const blob = new Blob(chunks);
+            recordView.src = URL.createObjectURL(blob);
+        };
+    } else if(btnRecord.textContent === 'stop') {
+        btnRecord.textContent = 'record';
+        mediaRecorder.stop();
+    }
+};
